9354a07d68b8fdc674ea6b5068eeece2324087d0
Taylor Mohney <taylormohneytech@gmail.com>
2023-04-18 09:38:02 -0700

    added dex branding

---
+++ Wonda is a DEX Brand Designer/Narrative.md
@@ -0,0 +1,7 @@
+# Outcome Quality
+
+Relatively High Quality
+
+# Narrative
+
+I assumed with such a vague objective the program would be likely to succumb to the decision paralysis it usually suffers when given research tasks. However, it remained relatively focused. Giving concrete suggestions on a color pallette to use and specific ideas for logos with some Dalle generated images. Although I wouldn't call it a perfect result the prompt actually generated some useful assets.
---
+++ Wonda is a DEX Brand Designer/milestone.py
@@ -0,0 +1,13 @@
+Save links to branding ideas:
+https://www.creativebloq.com/features/the-designers-guide-to-using-colour-in-branding
+https://www.canva.com/logos/color-psychology-the-logo-color-tricks-used-by-top-companies/
+https://blog.podium.com/colors-and-what-they-mean/
+Save potential color palettes:
+-Blue, green, and purple color scheme
+https://www.color-hex.com/color-palette/50022
+- Blue, reds, and yellows color scheme
+https://www.color-hex.com/color-palette/54838
+Save potential logo ideas:
+- Incorporating shapes like diamond or square to represent the value proposition conveyed by the project
+https://www.pinterest.nz/pin/754845587185305708/
+- (De)centralized signifiers
